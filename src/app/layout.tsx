cba1b2d07667626aa4c32501013e9e3eaf91e13d
ngxquang <baophong2401@gmail.com>
2024-10-07 10:38:48 +0700

    upload repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google"
+import DashboardWrapper from "./dashboardWrapper";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <DashboardWrapper>
+          {children}
+          </DashboardWrapper>
       </body>
     </html>
   );
