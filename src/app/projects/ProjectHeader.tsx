4c5463d6b68132128059be9c9bb7451f5aefe26e
ngxquang <baophong2401@gmail.com>
2024-10-25 11:21:46 +0700

    feat: Modal Create Project

--- src/app/projects/ProjectHeader.tsx
+++ src/app/projects/ProjectHeader.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Header from '@/components/Header';
-import { Clock, Clock10Icon, Filter, Grid3x3, List, Share2, Table } from 'lucide-react';
+import { Clock, Clock10Icon, Filter, Grid3x3, List, PlusSquare, Share2, Table } from 'lucide-react';
 import React, { act, useState } from 'react'
+import ModalNewProject from './ModalNewProject';
 
 type Props = {
     activeTab: string;
@@ -16,8 +17,21 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
   return (
     <div className='px-4 xl:px-6'>
         {/* Modal New Project */}
+        <ModalNewProject 
+            isOpen={isModalNewProjectOpen}
+            onClose={() => setIsModalNewProjectOpen(false)}
+        />
+
+
         <div className='pb-6 pt-6 lg:pb-4 lg:pt-8'>
-            <Header name='Product Design Development'/>
+            <Header name='Product Design Development'
+                buttonComponent={
+                    <button className='flex items-center rounded-md bg-blue-primary px-3 py-2 text-white'
+                    onClick={() => setIsModalNewProjectOpen(true)}>
+                        <PlusSquare className='mr-2 h-5 w-5'/> New Boards
+                    </button>
+                }
+            />
         </div>
 
         {/* Tabs */}
